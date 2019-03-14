f5e4a1a43d8859e75555332982c77f137b8e44bb
Artem Cherednichenko <artemcherednichenkovasikievich@gmail.com>
2019-03-14 21:58:29 +0200

    Add json handler

--- file_handler/json_handler.go
+++ file_handler/json_handler.go
@@ -1,15 +1,42 @@
 package file_handler
 
-import "gopherex/urlshort/config"
+import (
+	"encoding/json"
+	"fmt"
+	"gopherex/urlshort/config"
+	"io/ioutil"
+)
 
 type JsonHandler struct {
 	Settings config.Settings
 }
 
-func (s JsonHandler) GetFileContent() [] UrlRewriteInterface {
-	// TODO implement json content parser
-	var data [] UrlRewriteInterface
+func (handler JsonHandler) GetFileContent() [] UrlRewriteInterface {
+	file, err := ioutil.ReadFile(handler.Settings.GetFilePath())
+	if err != nil {
+		fmt.Println("Error during json file reading.")
+		panic(err)
+	}
+	data, err := handler.unpackJson(file)
+	if err != nil {
+		fmt.Println("Json content is invalid.")
+		panic(err)
+	}
 	return data
+}
+
+
+func (handler *JsonHandler ) unpackJson(jsonData []byte) ( [] UrlRewriteInterface, error) {
+	var out [] UrlMappingJson
+	if err := json.Unmarshal(jsonData, &out); err != nil {
+		return nil, err
+	}
+	// Re-convert items due to this https://github.com/golang/go/wiki/InterfaceSlice
+	items := make([]UrlRewriteInterface,len(out))
+	for i, item := range out {
+		items[i] = item
+	}
+	return items, nil
 }
 
 type UrlMappingJson struct  {
